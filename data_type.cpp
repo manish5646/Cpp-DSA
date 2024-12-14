4ee77dc6702ebecc13061c9a38518fb312b38e3d
Manish  Mishra <manishweb9199@gmail.com>
2024-12-15 02:12:55 +0530

    Add files via upload

---
+++ data_type.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /*int a = 123;
+   cout<< a << endl;
+
+   char b = 'v';
+   cout<< b << endl;
+
+
+int a= 'a';
+cout<< a << endl;
+
+char ch = 123456;
+cout<< ch << endl;
+
+char ch1 = 98;
+cout<< ch1 << endl;
+*/
+
+int a = 3;
+int b = 3;
+
+bool first = (a==b);
+cout<< first << endl;
+
+bool second = (a>b);
+cout << second << endl;
+
+bool third = (a>=b);
+cout << third << endl;
+
+bool fourth = (a!=b);
+cout<< fourth <<endl;
+
+
+}
