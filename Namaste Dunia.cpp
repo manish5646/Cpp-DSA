7daabceed479589c50122a6e88115e74bda74a2c
Manish  Mishra <manishweb9199@gmail.com>
2024-12-15 02:13:45 +0530

    Add files via upload

---
+++ Namaste Dunia.cpp
@@ -0,0 +1,13 @@
+/*#include<iostream>
+using namespace std;
+
+int main(){
+   cout << "Namaste Duniya" << endl;
+}*/
+#include<iostream>
+using namespace std;
+ int main(){
+     cout<<"namaste Duniya"<< endl;
+ }
+     
+ 
